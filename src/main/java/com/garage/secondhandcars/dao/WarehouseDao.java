f3ff3843f69ab89c3209a4e08469b9d90edb1ea2
forameng <forameng@users.noreply.github.com>
2021-02-13 21:33:33 +0100

    updated to run test cases

--- src/main/java/com/garage/secondhandcars/dao/WarehouseDao.java
+++ src/main/java/com/garage/secondhandcars/dao/WarehouseDao.java
@@ -2,6 +2,7 @@ package com.garage.secondhandcars.dao;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.Getter;
@@ -12,7 +13,7 @@ import lombok.Setter;
 @Table(name="warehouse")
 public class WarehouseDao {
 	@Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int warehouse_id;
 	@Column(name="name") 
 	private String name;
